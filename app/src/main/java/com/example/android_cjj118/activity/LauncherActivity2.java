e5a4da2aa0622196d6a6448ea6a7471dc162a824
CliffCedar <smallfasto@qq.com>
2024-05-12 09:21:39 +0800

    新增十二符咒

--- app/src/main/java/com/example/android_cjj118/activity/LauncherActivity2.java
+++ app/src/main/java/com/example/android_cjj118/activity/LauncherActivity2.java
@@ -33,8 +33,6 @@ public class LauncherActivity2 extends AppCompatActivity {
             }
             finish();
         },1000);
-        Glide.with(this).load("https://s1.aigei.com/src/img/gif/c4/c4cf89e1562344078ca8d9d8900cc68b.gif?imageMogr2/"
-                +"auto-orient/thumbnail/!282x282r/gravity/Center/crop/282x282/"
-                +"quality/85/%7CimageView2/2/w/282&e=1735488000&token=P7S2Xpzfz11vAkASLTkfHN7Fw-oOZBecqeJaxypL:smun44OF-yX2vtiMlx5cGKs93Bg=").preload();
+        Glide.with(this).load("https://s1.aigei.com/src/img/gif/c4/c4cf89e1562344078ca8d9d8900cc68b.gif?imageMogr2/auto-orient/thumbnail/!282x282r/gravity/Center/crop/282x282/quality/85/%7CimageView2/2/w/282&e=1735488000&token=P7S2Xpzfz11vAkASLTkfHN7Fw-oOZBecqeJaxypL:smun44OF-yX2vtiMlx5cGKs93Bg=").preload();
     }
 }
